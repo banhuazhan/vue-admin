25a92820dcbf1923ba871a797be10031a9b03400
banhuazhan <1948262451@qq.com>
2021-01-07 22:03:41 +0800

    login

---
+++ src/views/login/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="content">
+      测试
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Login",
+});
+</script>
+
+<style scoped lang="scss">
+@import "./style";
+</style>
